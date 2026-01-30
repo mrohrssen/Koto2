4c0661fe1dc987fbfd96d02f6fb0c0d4935ab37d
Michia Rohrssen <michia@Michias-MacBook-Pro.local>
2026-01-30 12:50:47 -0800

    fix: bundle anime.js to public/js/lib for browser access

--- public/js/ui/combat-effects.js
+++ public/js/ui/combat-effects.js
@@ -10,7 +10,7 @@
  * Effects are designed to layer - call multiple for combined impact.
  */
 
-import anime from '/node_modules/animejs/lib/anime.es.js';
+import anime from '../lib/anime.esm.min.js';
 
 // ============ CONFIGURATION ============
 
